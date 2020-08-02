5f56ff53d843fc503c3f3d6fa077972891439b99
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-08-01 23:06:01 -0500

    Primer commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
 
-class IndexPage extends React.Component {
+class IndexPage extends React.Component
+{
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Personal Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -17,19 +18,19 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+         Hola mundo{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Bienvenido a mi blog</p>
         <p>
           This starter comes out of the box with styled components and Gatsby's
           default starter blog running on Netlify CMS.
         </p>
         <p>Now go build something great!</p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px">Ir al blog</Button>
         </Link>
       </Layout>
     )
